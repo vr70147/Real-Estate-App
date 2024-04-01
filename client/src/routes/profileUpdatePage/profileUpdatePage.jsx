30f6403d631ce07b59b449c3016bc80b2b59897f
Raanan Adut <radesigner84@gmail.com>
2024-04-01 23:34:23 +0300

    update avatar

--- client/src/routes/profileUpdatePage/profileUpdatePage.jsx
+++ client/src/routes/profileUpdatePage/profileUpdatePage.jsx
@@ -1,16 +1,39 @@
-import { useContext } from 'react';
+import { useContext, useState } from 'react';
 import './profileUpdatePage.scss';
 import { AuthContext } from '../../context/AuthContext';
+import ApiReq from '../../lib/apiReq';
+import { useNavigate } from 'react-router-dom';
+import UploadWidget from '../../components/uploadWidgets/UploadWidget';
 
 function ProfileUpdatePage() {
   const { currentUser, updateUser } = useContext(AuthContext);
+  const [error, setError] = useState('');
+  const [avatar, setAvatar] = useState(currentUser.avatar);
+  const navigate = useNavigate();
 
-  const handleUpdate = () => {};
+  const handleUpdate = async (e) => {
+    e.preventDefault();
+    const formData = new FormData(e.target);
+    const { username, email, password } = Object.fromEntries(formData);
+    try {
+      const res = await ApiReq.put(`/users/${currentUser.id}`, {
+        username,
+        email,
+        password,
+        avatar,
+      });
+      updateUser(res.data);
+      navigate('/profile');
+    } catch (err) {
+      console.log(err);
+      setError(err.response.data.message);
+    }
+  };
 
   return (
     <div className="profileUpdatePage">
       <div className="formContainer">
-        <form>
+        <form onSubmit={handleUpdate}>
           <h1>Update Profile</h1>
           <div className="item">
             <label htmlFor="username">Username</label>
@@ -35,13 +58,20 @@ function ProfileUpdatePage() {
             <input id="password" name="password" type="password" />
           </div>
           <button onSubmit={handleUpdate}>Update</button>
+          {error && <span>error</span>}
         </form>
       </div>
       <div className="sideContainer">
-        <img
-          src={currentUser.avatar || '/noavatar.jpg'}
-          alt=""
-          className="avatar"
+        <img src={avatar || '/noavatar.jpg'} alt="" className="avatar" />
+        <UploadWidget
+          uwConfig={{
+            cloudName: 'supermarketapp',
+            uploadPreset: 'estate',
+            multiple: false,
+            maxImageFileSize: 2000000,
+            folder: 'avatars',
+          }}
+          setAvatar={setAvatar}
         />
       </div>
     </div>
