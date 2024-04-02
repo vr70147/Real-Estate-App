4fb91db99ec7b0fb77b2cbf9e55ba3846fedff84
Raanan Adut <radesigner84@gmail.com>
2024-04-02 23:52:46 +0300

    create post CRUD on API

--- api/routes/post.route.js
+++ api/routes/post.route.js
@@ -1,9 +1,18 @@
 import express from 'express';
-
+import { verifyToken } from '../middleware/verifyToken.js';
+import {
+  addPosts,
+  deletePosts,
+  getPost,
+  getPosts,
+  updatePosts,
+} from '../controllers/post.controller.js';
 const router = express.Router();
 
-router.get('/test', (req, res) => {
-  console.log('router works!');
-});
+router.get('/', getPosts);
+router.get('/:id', getPost);
+router.post('/', verifyToken, addPosts);
+router.put('/:id', verifyToken, updatePosts);
+router.delete('/:id', verifyToken, deletePosts);
 
 export default router;
